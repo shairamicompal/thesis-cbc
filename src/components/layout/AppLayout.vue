b9f65e3e27c726282693b35c7dd8e3f300fafc01
Shaira Micompal <shairamicompal@gmail.com>
2025-08-09 03:32:36 +0800

    feat: updated applayout and views

---
+++ src/components/layout/AppLayout.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref } from 'vue'
+import { RouterLink } from 'vue-router'
+
+const theme = ref('light')
+
+function onClick() {
+  theme.value = theme.value === 'light' ? 'dark' : 'light'
+}
+</script>
+
+<template>
+  <v-responsive class="border rounded">
+    <v-app :theme="theme">
+      <v-app-bar
+        class="px-3"
+        :color="theme === 'light' ? 'blue-lighten-3' : 'blue-darken-1'"
+        border
+      >
+        <v-spacer></v-spacer>
+
+        <v-btn
+          :icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+          variant="elevated"
+          slim
+          @click="onClick"
+        ></v-btn>
+      </v-app-bar>
+
+      <v-main>
+        <v-container>
+          <slot name="content"></slot>
+        </v-container>
+      </v-main>
+
+      <v-footer :color="theme === 'light' ? 'red-darken-4' : 'red-darken-1'" border app
+        >2024 - Hematuklas</v-footer
+      >
+    </v-app>
+  </v-responsive>
+</template>
--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,80 +1,61 @@
 <script setup>
-import { ref } from 'vue'
-import { RouterLink } from 'vue-router';
+import AppLayout from '@/components/layout/AppLayout.vue'
 
-const theme = ref('light')
+import { useDisplay } from 'vuetify'
 
-function onClick() {
-  theme.value = theme.value === 'light' ? 'dark' : 'light'
-}
+const { mobile} = useDisplay()
+
 </script>
 
 <template>
-  <v-responsive class="border rounded">
-    <v-app :theme="theme">
-      <v-app-bar class="px-3" color="blue-lighten-3">
-        <v-spacer></v-spacer>
+  <AppLayout>
+    <template #content>
+      <v-row>
+        <v-col cols="12" md="6" class="mx-auto">
+          <v-card class="mx-auto" elevation="24">
+            <v-card-title>
+              <v-img
+                src="/images/logo-favicon.png"
+               :width="mobile ? '50%' : '25%'"
+                class="mx-auto"
+              ></v-img>
+              <h3 class="font-weight-black text-center">HemaTuklas</h3>
+              <!-- <h4 class="text-center">login</h4> -->
 
-        <v-btn
-          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
-          text="Toggle Theme"
-          slim
-          @click="onClick"
-        ></v-btn>
-      </v-app-bar>
+              <v-divider class="my-4"></v-divider>
+            </v-card-title>
 
-      <v-main>
-        <v-container>
-          <v-row>
-            <v-col cols="12" md="6" class="mx-auto">
-              <v-card class="mx-auto" prepend-icon="mdi-account-plus" subtitle="Register your account" elevation="24">
-                <template v-slot:title>
-                  <span class="font-weight-black">Hematuklas</span>
-                </template>
+            <v-card-text class="bg-surface-light pt-4">
+              <v-form fast-fail @submit.prevent>
+                <v-text-field label="Firstname" variant="outlined"></v-text-field>
 
-                <v-card-text class="bg-surface-light pt-4">
-                  <v-form fast-fail @submit.prevent>
-                    <v-text-field
-                      label="Firstname"
-                      variant="outlined"
-                    ></v-text-field>
+                <v-text-field label="Lastname" variant="outlined"></v-text-field>
 
-                    <v-text-field
-                      label="Lastname"
-                      variant="outlined"
-                    ></v-text-field>
+                <v-text-field label="Email" variant="outlined"></v-text-field>
 
-                    <v-text-field
-                      label="Email"
-                      variant="outlined"
-                    ></v-text-field>
+                <v-text-field label="Password" variant="outlined" type="password"></v-text-field>
 
-                    <v-text-field
-                      label="Password"
-                      variant="outlined"
-                      type="password"
-                    ></v-text-field>
+                <v-text-field
+                  label="Password Confirmation"
+                  variant="outlined"
+                  type="password"
+                ></v-text-field>
 
-                    <v-text-field
-                      label="Password Confirmation"
-                      variant="outlined"
-                      type="password"
-                    ></v-text-field>
+                <v-btn class="bg-light-blue-darken-4" type="submit" block prepend-icon="mdi-account-plus"
+                  >REGISTER</v-btn
+                >
+              </v-form>
 
-                    <v-btn type="submit" block prepend-icon="mdi-account-plus">REGISTER</v-btn>
-                  </v-form>
+              <v-divider class="my-4"></v-divider>
 
-                  <v-divider class="my-4"></v-divider>
-
-                  <h5 class="text-center">Already have an account? <RouterLink to="/" class="text-primary">Click here to Login</RouterLink></h5>
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-main>
-
-      <v-footer color="red-darken-3" border app class="text-center">2024 - Hematuklas</v-footer>
-    </v-app>
-  </v-responsive>
+              <h5 class="text-center">
+                Already have an account?
+                <RouterLink to="/" class="text-red-lighten-1">Click here to Login</RouterLink>
+              </h5>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </template>
+  </AppLayout>
 </template>
